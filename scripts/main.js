ab9fa2072477731e5b3df606a085e1b742a6ffb4
Marcus <marcus.lara@hotmail.com>
2023-05-26 15:04:34 -0300

    Implementando a lógica para as medidas dos masteriais

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,28 @@
 const formulario = document.getElementById('form');
 
 let longarinas;
-let longarinasqQtd;
+let longarinasqQtd = 2;
 
 let pranchas;
-let pranchasQtd;
+let pranchasQtd = 3;
 
 let molduraFrontalHor;
-let molduraFrontalHorQtd;
+let molduraFrontalHorQtd = 4;
 
 let molduraFrontalVert;
-let molduraFrontalVertQtd;
+let molduraFrontalVertQtd = 4;
 
 let molduraLatHor;
-let molduraLatHorQtd;
+let molduraLatHorQtd = 4;
 
 let molduraLatVert;
-let molduraLatVertQtd;
+let molduraLatVertQtd = 4;
 
 let molduraTopoVert;
-let molduraTopoVertQtd;
+let molduraTopoVertQtd = 2;
 
 let molduraTopoHor;
-let molduraTopoHorQtd;
+let molduraTopoHorQtd = 2;
 
 
 formulario.addEventListener('submit', (evento) => {
@@ -33,10 +33,30 @@ formulario.addEventListener('submit', (evento) => {
     const altura = parseInt(document.getElementById('altura').value);
 
     longarinas = profundidade + 50;
-    console.log(longarinas);
 
     pranchas = comprimento;
+
+    molduraLatHor = comprimento + 50;
+
+    molduraLatVert = altura + 21 - 100;
+
+    molduraFrontalHor = profundidade;
+
+    molduraFrontalVert = altura + 66 - 100;
+
+    molduraTopoVert = molduraLatHor;
+
+    molduraTopoHor = profundidade - 50;
     
+    console.log("Longarina: " + longarinas);
+    console.log("Prancha: " + pranchas);
+    console.log("molduraLatHor: " + molduraLatHor);
+    console.log("molduraLatVert: " + molduraLatVert);
+    console.log("molduraFrontalHor: " + molduraFrontalHor);
+    console.log("molduraFrontalVert: " + molduraFrontalVert);
+    console.log("molduraTopoVert: " + molduraTopoVert);
+    console.log("molduraTopoHor: " + molduraTopoHor);
+
 
     
 })
